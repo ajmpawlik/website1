9c2cc6571c0ddcbfe11825cf5c57c755710e08dc
Aleksandra <ajmpawlik@gmail.com>
2019-09-14 23:54:35 +0100

    feature: step 6 style the workspace with the caption in a wrong location

---
+++ public/javascripts/application.js
@@ -0,0 +1,23 @@
+$(document).on('submit', '#search-form', function(e) {
+  e.preventDefault();
+  var options = {
+    term: $('#search-term').val(),
+    resultsContainer: $('#search-results')
+  };
+  findImagesOnGoogle(options);
+});
+
+$(document).on('click', '#search-results img', function() {
+  var url = $(this).data('url');
+  $("#workspace img").remove();
+  var img = $("<img>").attr('src', url);
+  $("#workspace").append(img);
+});
+
+$(document).on('ready', function(){
+  $("#caption").text($('#caption-text').val());
+});
+
+$(document).on('input', '#caption-text', function(){
+  $("#caption").text($(this).val());
+});
